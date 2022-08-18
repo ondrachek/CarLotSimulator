8f4b575118d3a85089cde0ccfd319813c60a0fcd
Alexander Ondrachek <ondrachek@gmail.com>
2022-08-17 20:45:56 -0500

    initial commit

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -9,14 +9,57 @@ namespace CarLotSimulator
             //TODO
 
             //Create a seperate class file called Car
+            public class Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            public int Year { get; set; }
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public string EngineNoise { get; set; }
+            public string HonkNoise { get; set; }
+            public bool IsDrivable { get; set; }
+
+
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise() }
+        //The methods should take one string parameter: the respective noise property
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+        public void MakeEngineNoise(string HonkNoise)
+        {
+            Console.WriteLine(HonkNoise);
+
+
 
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var focus = new Car();
+            focus.Make = "Ford";
+            focus.Model = "focus";
+            focus.Year = 2013;
+            focus.EngineNoise = "vroom";
+            focus.HonkNoise = "quiet";
+            focus.IsDrivable = false;
+
+            var hisCar = new Car()
+            {
+                Year = 2012,
+                Make = "ford",
+                Model = "F150",
+                EngineNoise = "prrrrr",
+                HonkNoise = "beeeeeeeeeeep",
+                IsDriveable = True
+            };
+
+            var herCar = new Car();
+            herCar.Make = "Toyota"
+                herCar.Model = "highlander"
+                herCar.Year = 2018;
+            herCar.EngineNoise = "grrrrrrr";
+            herCar.HonkNoise = "loud";
+            herCar.IsDrivable = true;
 
             //*************BONUS*************//
 
